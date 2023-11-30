6ed26b01e3f2c28e2cd181027e9e919b3150288c
Nexus-eternal <133900069+Nexus-eternal@users.noreply.github.com>
2023-11-30 13:42:06 +0100

    added tent sprite; Modifyed Main

--- main.py
+++ main.py
@@ -1,11 +1,51 @@
 import tkinter as tk
-from scenes import *
+from PIL import Image, ImageTk
+import scenes as sc
 
+class Scene:
+    def __init__(self, root, image_path, text_path, command_1, command_2, option_1, option_2):
+        self.root = root
+        self.mainframe = tk.Frame(self.root)
+        self.mainframe.pack()
+        self.image_path = image_path
+        self.text_path = text_path
+
+        self.command_1 = command_1
+        self.command_2 = command_2
+        self.option_1 = option_1
+        self.option_2 = option_2
+    
+    def draw_scene(self):
+        # Clear everything from previous scene
+        sc.clear_frame(self.mainframe)
+
+        # Unpacking image and placing it
+        image = Image.open(self.image_path)
+        tk_image = ImageTk.PhotoImage(image)
+        self.image_label = tk.Label(self.mainframe)
+        self.image_label.config(image=tk_image)
+        self.image_label.image = tk_image
+        self.image_label.grid(row=0, column=0, columnspan=2)
+
+        # Unpacking text
+        file = open(self.text_path, "r", encoding="utf-8" )
+        content = file.read()
+
+        text_label = tk.Label(self.mainframe, text=content)
+        text_label.grid(row=1, column=0, columnspan=2)
+
+        # Buttons
+        button1 = tk.Button(self.mainframe, text=f"{self.option_1}", command=lambda: exec(f'{self.command_1}'))
+        button1.grid(row=2, column=0)
+        button2 = tk.Button(self.mainframe, text=f"{self.option_2}", command=lambda: exec(f'{self.command_2}'))
+        button2.grid(row=2, column=1)
 
 root = tk.Tk()
 
-scene1 = Scene(root, "images\warchief.jpg", "ABOBA", None, None)
-scene1.draw_scene()
+Intro = Scene(root, 'images\_tent.jpg', "scenes_text\Into.txt", "scene1a.draw_scene()", "scene1a.draw_scene()", "Aboba", "Baobab")
+scene1a = Scene(root, 'images\warchief.jpg', "scenes_text\Into.txt", Intro, Intro, "AAAAAAA", "BBBBBB")
+Intro.draw_scene()
+
 
 root.mainloop()
 
--- scenes.py
+++ scenes.py
@@ -1,36 +1,5 @@
 import tkinter as tk
-from PIL import Image, ImageTk
 
 def clear_frame(frame):
-   for widgets in frame.winfo_children():
-      widgets.destroy()
-
-class Scene:
-    def __init__(self, root, image_path, text_path, to_scene_1, to_scene_2):
-        self.root = root
-        self.mainframe = tk.Frame(self.root)
-        self.mainframe.pack()
-        self.image_path = image_path
-        self.text_path = text_path
-        self.to_scene_1 = to_scene_1
-        self.to_scene_2 = to_scene_2
-    
-    def draw_scene(self):
-        #Clear everything from previous scene
-        clear_frame(self.mainframe)
-
-        # Unpacking image and placing it
-        image = Image.open(self.image_path)
-        tk_image = ImageTk.PhotoImage(image)
-        self.image_label = tk.Label(self.mainframe)
-        self.image_label.config(image=tk_image)
-        self.image_label.image = tk_image
-        self.image_label.grid(row=0, column=0, columnspan=2)
-
-        #Unpacking text
-        file = open("scenes_text\Into.txt", "r", encoding="utf-8" )
-        content = file.read()
-        print(content)
-
-        text_label = tk.Label(self.mainframe, text=content)
-        text_label.grid(row=1, column=0, columnspan=2)
+    for widgets in frame.winfo_children():
+        widgets.destroy()
